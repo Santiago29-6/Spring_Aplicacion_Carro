69533d9151ce91ef54744003a411683cd62212d4
Santiago29-6 <chavarrokevin29@gmail.com>
2024-03-04 14:50:55 -0500

    Comenzando proyecto

---
+++ src/main/java/com/ks/CarroartifactApplication.java
@@ -0,0 +1,13 @@
+package com.ks.carros.carroartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ks/carros/carroartifact/CarroartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.carros.carroartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
